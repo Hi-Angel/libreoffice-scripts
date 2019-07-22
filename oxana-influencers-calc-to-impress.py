1a0a1817e1999f23fe89778a582cbcc0ac2c7111
Konstantin Kharlamov <Hi-Angel@yandex.ru>
2019-07-23 01:53:11 +0300

    oxana-influencers-calc-to-impress.py: some fixes and test code
    Signed-off-by: Konstantin Kharlamov <Hi-Angel@yandex.ru>

--- oxana-influencers-calc-to-impress.py
+++ oxana-influencers-calc-to-impress.py
@@ -24,10 +24,13 @@ def getLOInstances(desktop):
     assert(CALC_STR in loInstances[1].ImplementationName)
     return loInstances
 
+def tablesFromSlide(slide):
+    return [item for item in slide if item.ShapeType == 'com.sun.star.drawing.TableShape']
+
 def collectNamesImpress(slides):
     names = {} # String : Row
     for slide in slides:
-        tables = [item for item in slide if item.ShapeType == 'com.sun.star.drawing.TableShape']
+        tables = tablesFromSlide(slide)
         for table in tables:
             for row in table.Model.Rows:
                 names[row.getCellByPosition(1, 0).String] = row
@@ -46,10 +49,10 @@ def setFilters(filters, filtersToSet):
     for wantedFilterNames in filtersToSet:
         filter = namesToFilters.pop(wantedFilterNames)
         if filter.IsHidden != False:
-            filter.IsHidden == False
+            filter.IsHidden = False
     for _,filter in namesToFilters.items():
         if filter.IsHidden != True:
-            filter.IsHidden == True
+            filter.IsHidden = True
 
 # OutputRange of pivot table includes "technical rows" at the beginning and end
 def pivotTableUsedRangeMentions(pivotTable, sheet):
@@ -61,17 +64,26 @@ def pivotTableUsedRangeMentions(pivotTable, sheet):
                                         range_raw.EndColumn,
                                         range_raw.EndRow + N_TECHNICAL_ROWS_AT_END)
 
+# Rows -> Int -> [String]
+def rowToStrings(row, ncols):
+    ret = []
+    for iCol in range(0, ncols):
+        ret.append(row.getCellByPosition(iCol, 0).String)
+    return ret
+
 # Sheet -> [String] -> [(int, Row)]; where Int is a number of views, and Row is the
 # row
 def collectFromPivotTable(sheet, filterNames):
     pilotTable = sheet.DataPilotTables.getByIndex(0)
     filters = pilotTable.DataPilotFields.getByName('Author type').Items
-    # "influencers" means publishers and celebrity
+    # "influencers" means bloggers and celebrity
     setFilters(filters, filterNames)
     ret = []
-    for row in pivotTableUsedRangeMentions(pilotTable, sheet).Rows:
+    pivotRange = pivotTableUsedRangeMentions(pilotTable, sheet)
+    for row in pivotRange.Rows:
         mb_views = row.getCellByPosition(1, 0).String # aka impressions
-        ret.append((int(mb_views.partition(',')[0]) if mb_views else 0, row)) # 0 is required for sorting
+        ret.append((int(mb_views.partition(',')[0]) if mb_views else 0, # 0 is required for sorting
+                    rowToStrings(row, pivotRange.Columns.Count)))
     ret.sort(key = lambda pair: pair[0], reverse = True) # most views first
     return ret
 
@@ -81,12 +93,30 @@ def collectPublishers(sheet):
 
 # [(Int, Row)],
 def collectInfluencers(sheet):
-    return collectFromPivotTable(sheet, ['Publisher', 'Celebrity'])
+    return collectFromPivotTable(sheet, ['Blogger', 'Celebrity'])
 
 desktop = connectToLO()
 (drawApp, spreadsheetApp) = getLOInstances(desktop)
 mentionsSheet = spreadsheetApp.Sheets.getByName('QQ')
 influencersSorted = collectInfluencers(mentionsSheet)
 publishersSorted  = collectPublishers(mentionsSheet)
+
+# test code below
+slide1 = drawApp.DrawPages.getByIndex(0)
+table1 = tablesFromSlide(slide1)[0]
+index = 0
+for row in table1.Model.Rows:
+    if index == 0:
+        index += 1
+        continue
+    if index >= len(influencersSorted):
+        break
+    row.getCellByPosition(1, 0).String = influencersSorted[index][1][0] # name
+    views = influencersSorted[index][0]
+    views_str = str(int(views / 1000)) + ' K' if views > 1000 else str(views)
+    row.getCellByPosition(2, 0).String = views_str
+    index += 1
+
+
 # fillTable(influencersSorted, getInflTable(mentionsSheet))
 # fillTable(publishersSorted, getPublTable(mentionsSheet))
